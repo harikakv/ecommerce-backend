5c45eeef2a49ecb9c0bf432186e829e987098ca7
harikakv <harikakv059@gmail.com>
2026-01-15 18:23:09 +0530

    Fix CORS for Vercel frontend

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -139,12 +139,10 @@ REST_FRAMEWORK = {
     ],
 }
 
-CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOW_CREDENTIALS = True
 
 CSRF_TRUSTED_ORIGINS = [
     "https://*.vercel.app",
     "https://ecom-frontend-kothapally-vallepu-harikas-projects.vercel.app",
 ]
 
-
+CORS_ALLOW_CREDENTIALS = True
