4737166449a8028bf6f91b9c0d45cf9f3e637ce0
harikakv <harikakv059@gmail.com>
2026-01-15 20:39:35 +0530

    Change url

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('api/token/', obtain_auth_token),
 
     #  APPS
-    path('api/products/', include('products.urls')),
+    path('api/products', include('products.urls')),
     path('api/users/', include('users.urls')),
     path('api/cart/', include('cart.urls')),
     path('api/orders/', include('orders.urls')),
